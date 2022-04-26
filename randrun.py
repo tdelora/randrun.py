2d843b02451fddf90be56f15b428e98ab7cbade4
Thomas De Lora <TED@Sardi.local>
2022-04-26 14:56:14 -0700

    first commit of randrun.py

---
+++ randrun.py
@@ -0,0 +1,15 @@
+import random
+
+def  main():
+
+    count = 100
+
+    while count:
+        rNum = random.randint(1,9)
+
+        print(rNum)
+        count-=1
+
+
+if __name__ == '__main__':
+    main()
