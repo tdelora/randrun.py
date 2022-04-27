0cd8e00874fa064a02be0bbb3ab0dcc6e848121c
Thomas De Lora <TED@Sardi.local>
2022-04-26 18:57:15 -0700

    Added rr_init_rand which allows seeds to be set for rand

--- randrun.py
+++ randrun.py
@@ -3,6 +3,8 @@ import rr_random
 def  main():
 
     count = 100
+    
+    # rr_random.rr_init_rand(10)
 
     while count:
         rNum = rr_random.rr_rand_lim(9)
--- rr_random.py
+++ rr_random.py
@@ -1,4 +1,7 @@
 import random
 
+def rr_init_rand(seed):
+    random.seed(seed)
+
 def rr_rand_lim(upperLimit):
     return(random.randint(1,upperLimit))
