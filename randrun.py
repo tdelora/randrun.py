b578dc2363ef1b20823ca0890a0f0b7c1a494bb7
Thomas De Lora <TED@Sardi.local>
2022-05-04 21:30:08 -0700

    Added argument parsing to randrun.py

--- randrun.py
+++ randrun.py
@@ -1,10 +1,18 @@
+import argparse
 import rr_random, rr_nats_nouns_verbs
 
 def  main():
+    parser = argparse.ArgumentParser()
 
-    count = 100
+    parser.add_argument('--maxSteps',default=100, help = "The maximum number of steps to run.")
+    parser.add_argument('--randomSeed',default=0, help = "The value to use for random seed.")
+    args = parser.parse_args()
+
+    count = int(args.maxSteps)
+    randSeed = int(args.randomSeed)
     
-    # rr_random.rr_init_rand(10)
+    if randSeed != 0:
+        rr_random.rr_init_rand(randSeed)
 
     while count:
         natsNoun = rr_nats_nouns_verbs.rr_get_randon_nats_noun()
